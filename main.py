3431181afe3300b2f13039ce6f1ad531a4bcd069
ameliekou <ka7675@mci4me.at>
2024-06-23 17:24:49 +0200

    BMI,Layout,Textfeld

--- main.py
+++ main.py
@@ -27,18 +27,25 @@ if 'picture_path' not in st.session_state:
 if 'ekg_test' not in st.session_state:
     st.session_state.ekg_test = None
 
+## Session State für Notizen je Nutzer
+if 'user_notes' not in st.session_state:
+    st.session_state.user_notes = {}
+
 with ekg_tab:
 
     # Schreibe die Überschrift
     st.write("# EKG APP")
-    st.write("## Versuchsperson auswählen")
-
-    # Auswahlbox, wenn Personen anzulegen sind
-    st.session_state.aktuelle_versuchsperson = st.selectbox(
-        'Versuchsperson',
-        options = person_names, key="sbVersuchsperson", label_visibility="hidden")
+    
 
     col_left, col_right = st.columns(2)
+    with col_left:
+        st.write("##### Versuchsperson auswählen:")
+    
+    # Auswahlbox, wenn Personen anzulegen sind
+    with col_right:
+        st.session_state.aktuelle_versuchsperson = st.selectbox(
+            'Versuchsperson',
+            options = person_names, key="sbVersuchsperson", label_visibility="hidden")
 
     # Name der Versuchsperson
     #st.write("Der Name ist: ", st.session_state.aktuelle_versuchsperson) 
@@ -47,11 +54,22 @@ with ekg_tab:
     person = db.find_person_data_by_name(st.session_state.aktuelle_versuchsperson)
 
     with col_right:
-        st.write("## Daten von ", st.session_state.aktuelle_versuchsperson)
         st.write("Geburtsdatum: ", person.date_of_birth)
         st.write("Alter: ", person.calc_age())
         st.write("ID: ", person.id)
-        st.write("maximale Herzfrequenz: ", person.calc_max_heart_rate())
+        
+        #Öffne EKG-Daten
+        ekgdata_dict = ekgdata.EKGdata.load_by_id(person.id)
+
+    # TODO: Für eine Person gibt es ggf. mehrere EKG-Daten. Diese müssen über den Pfad ausgewählt werden können
+        ekg_list = []
+        for ekg in ekgdata_dict:
+            ekg_list.append(ekg["id"])
+
+    # Auswahlbox für EKG-Daten
+        st.session_state.ekg_test = st.selectbox(
+            'EKG-Test auswählen:',
+            options = ekg_list)
 
 
 
@@ -67,29 +85,27 @@ with ekg_tab:
 
     with col_left:
         st.image(image, caption=st.session_state.aktuelle_versuchsperson)
+   
+   # Textfeld für Notizen
+    notes_key = f"notes_{person.id}_{st.session_state.aktuelle_versuchsperson}"
+    st.session_state.user_notes.setdefault(notes_key, "")
+    user_notes = st.text_area(
+        'Platz für Notizen:',
+        st.session_state.user_notes.get(notes_key, "")
+    )
+    st.session_state.user_notes[notes_key] = user_notes
 
-    #Öffne EKG-Daten
-    ekgdata_dict = ekgdata.EKGdata.load_by_id(person.id)
-
-    st.write("## EKG-Daten auswählen")
-    # TODO: Für eine Person gibt es ggf. mehrere EKG-Daten. Diese müssen über den Pfad ausgewählt werden können
-    ekg_list = []
-    for ekg in ekgdata_dict:
-        ekg_list.append(ekg["id"])
-
-    # Auswahlbox für EKG-Daten
-    st.session_state.ekg_test = st.selectbox(
-        'EKG-Test',
-        options = ekg_list)
 
     # EKG-Daten anzeigen
     current_ekg_data = ekgdata.EKGdata.load_by_id(person.id, st.session_state.ekg_test)
     current_ekg_data_class = ekgdata.EKGdata(current_ekg_data)
 
-    st.write("## EKG-Daten")
+    st.write("##### EKG-Daten")
     st.write("Datum: ", current_ekg_data["date"])
     st.write("dauer in Sekunden:", int(current_ekg_data_class.time_in_seconds()))
     st.write("durchschnittliche Herzfrequenz: ", int(current_ekg_data_class.mean_heart_rate()))
+    st.write("maximale Herzfrequenz: ", person.calc_max_heart_rate())
+
 
     # EKG-Daten als Matplotlib Plot anzeigen
     fig = current_ekg_data_class.plot_time_series()
@@ -101,11 +117,13 @@ with ekg_tab:
     st.slider("bis", 0, current_ekg_data_class.max_time(), current_ekg_data_class.max_time() - 1)
 
 
-
-
 with bmi_taab:
     gewicht = st.number_input("Gewicht in kg")
     groesse = st.number_input("Größe in cm")
 
-    if st.button("BMI berechnen"):
-        st.write("Der BMI beträgt: ", gewicht / (groesse/100)**2)
+    #if st.button("BMI berechnen"):
+        #st.write("Der BMI beträgt: ", gewicht / (groesse/100)**2)
+    if groesse > 0 and gewicht > 0:
+            bmi = gewicht / (groesse / 100) ** 2
+            bmi_rounded = round(bmi, 1)
+            st.write("Der BMI beträgt: {:.1f}".format(bmi_rounded))
